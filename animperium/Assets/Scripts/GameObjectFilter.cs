8a4272259a5a829f55a14f747afbe5b37b2efc50
Fritz Lallinger <flallinger@googlemail.com>
2016-04-14 00:12:13 +0200

    camerafocus-, inputmanager-update

---
+++ animperium/Assets/Scripts/GameObjectFilter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectFilter {
+
+	// Use this for initialization
+	public GameObjectFilter() {
+	
+	}
+	
+	public string TypeOfGameObject(GameObject g)
+	{
+		if(g.name.Contains("Hex"))
+			return "Hex";
+
+		return null;
+		}
+}
---
+++ animperium/Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	GameObjectAction actionManager;
+
+	// Use this for initialization
+	void Start () {
+		actionManager = new GameObjectAction();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ListenInput ();
+	}
+
+	private void ListenInput()
+	{
+		ListenMouseInput ();
+	}
+
+	private void ListenMouseInput()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			RaycastFromMouse ();
+			}
+	}
+
+
+	private void RaycastFromMouse()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit)) {
+			actionManager.Action (hit.collider.gameObject);
+		}
+		
+	}
+}
