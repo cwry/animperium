0dcef3e471c385baa5828515855398f45938a1fe
cwry <daniel.pochert@gmail.com>
2016-04-14 02:53:59 +0200

    turnmanager

---
+++ animperium/Assets/Scripts/TurnManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager {
+
+    static TurnManager instance;
+
+    public int turnID = 0;
+    public GameEvent onTurnBegin = new GameEvent();
+    public GameEvent onTurnEnd = new GameEvent();
+
+    private TurnManager(){
+        //SINGLETON
+    }
+
+    public TurnManager getInstance(){
+        return instance == null ? new TurnManager() : instance;
+    }
+
+    public void endTurn(){
+        onTurnEnd.fire(turnID);
+        turnID++;
+        onTurnBegin.fire(turnID);
+    } 
+}
