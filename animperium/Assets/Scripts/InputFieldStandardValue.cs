38ee54dd972f2983f576b8ce939ccebd936fffc1
Mesuo <flallinger@googlemail.com>
2016-04-23 03:16:31 +0200

    Ui Connect

---
+++ animperium/Assets/Scripts/InputFieldStandardValue.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InputFieldStandardValue : MonoBehaviour {
+
+    InputField inputField;
+	// Use this for initialization
+	void Start () {
+        inputField = GetComponent<InputField>();
+        inputField.text = "7777";
+        if(gameObject.name.Contains("IP"))
+        {
+            inputField.text = "127.0.0.1";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- animperium/Assets/Scripts/Networking/NetworkLobbyInput.cs
+++ animperium/Assets/Scripts/Networking/NetworkLobbyInput.cs
@@ -1,22 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using UnityEngine.UI;
 
 public class NetworkLobbyInput : MonoBehaviour {
 
     bool isConnected = false;
-	
-	void Update() { 
-        if(Input.GetKeyDown(KeyCode.S) && isConnected && NetworkData.server != null){
-            NetworkData.server.initGame(100, 100, (int)Random.Range(0, int.MaxValue));
+    public GameObject hostPortInput;
+    public GameObject joinPortInput;
+    public GameObject joinIPInput;
+    public GameObject testModeToggle;
+
+    int hostPort = 7777;
+    int joinPort = 7777;
+    int n;
+    string ip = "127.0.0.1";
+    bool isTest = true;
+    
+    public void SetIP()
+    {
+        ip = joinIPInput.GetComponent<InputField>().text;
+    }
+
+    public void SetIsTest()
+    {
+        isTest = testModeToggle.GetComponent<Toggle>().isOn;
+    }
+    public void SetHostPort()
+    {
+       if(Int32.TryParse(hostPortInput.GetComponent<InputField>().text, out n))
+        {
+            hostPort = n;
         }
-        if (isConnected) return;
-        if (Input.GetKeyDown(KeyCode.H)){
-            NetworkData.server = new GameServer(7777);
-            NetworkData.client = new GameClient("127.0.0.1", 7777);
-            isConnected = true;
-        } else if (Input.GetKeyDown(KeyCode.J)){
-            NetworkData.client = new GameClient("127.0.0.1", 7777);
-            isConnected = true;
+       
+    }
+
+    public void SetJoinPort()
+    {
+        if (Int32.TryParse(joinPortInput.GetComponent<InputField>().text, out n))
+        {
+            joinPort = n;
         }
-	}
+    }
+    public void InitServer()
+    {
+        NetworkData.server = new GameServer(hostPort);
+        NetworkData.client = new GameClient(ip, hostPort);
+        isConnected = true;
+    }
+
+    public void StartGame()
+    {
+        if (isConnected)
+        {
+            NetworkData.server.initGame(100, 100, (int)UnityEngine.Random.Range(0, int.MaxValue), isTest);
+        }
+    }
+
+    public void JoinServer()
+    {
+        NetworkData.client = new GameClient(ip, joinPort);
+        isConnected = true;
+    }
 }
