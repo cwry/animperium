2143438c0b5e286f424567051f2f52026fdeb443
Fritz Lallinger <flallinger@googlemail.com>
2016-05-02 13:25:19 +0200

    a bit ui

---
+++ animperium/Assets/ContextMenuSpawn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ContextMenuSpawn : MonoBehaviour {
+
+    public GameObject contextMenuPrefab;
+    private GameObject contextMenu; 
+    public GameObject canvas;
+    
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(SelectionManager.selectedTile != null && Input.GetMouseButtonDown(0))
+        {
+            if (contextMenu != null)
+            {
+                GameObject [] arr =  GameObject.FindGameObjectsWithTag("Ui");
+                foreach (GameObject g in arr)
+                {
+                    if(g.name.Contains(contextMenuPrefab.name))
+                    {
+                        Destroy(g);
+                    }
+                }
+            }
+            contextMenu = Instantiate(contextMenuPrefab, Camera.main.WorldToScreenPoint(SelectionManager.selectedTile.transform.position), Quaternion.identity) as GameObject;
+            contextMenu.transform.parent = canvas.transform;
+        }
+
+	}
+}
