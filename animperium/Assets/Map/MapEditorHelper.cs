a343bb8aa76abbd0fd9021d9d358438aceb16ff4
Fritz Lallinger <flallinger@googlemail.com>
2016-06-10 06:27:08 +0200

    test

---
+++ animperium/Assets/Map/MapEditorHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class MapEditorHelper : MonoBehaviour {
+    public GameObject editorGrid;
+    public Material trueMat;
+    public Material falseMat;
+
+    void Update(){
+        TileInfo[] tiles = editorGrid.GetComponentsInChildren<TileInfo>();
+        foreach(TileInfo tile in tiles){
+            tile.gameObject.GetComponent<Renderer>().material = tile.traversable ? trueMat : falseMat;
+        }
+    }
+}
