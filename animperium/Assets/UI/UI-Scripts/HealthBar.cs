19725b3e81f49707dad13f49f18916da6e15bb77
Fritz Lallinger <flallinger@googlemail.com>
2016-06-10 06:14:08 +0200

    HealthBar

---
+++ animperium/Assets/UI/UI-Scripts/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar: MonoBehaviour
+{
+    public GameObject foreGround;
+    public GameObject unitObject;
+    private Unit unit;
+    Vector3 localScale;
+
+    void Start()
+    {
+        localScale = foreGround.transform.localScale;
+        unit = unitObject.GetComponent<Unit>();
+    }
+
+    void Update()
+    {
+        foreGround.transform.localScale = new Vector3((unit.hitPoints / unit.maxHitPoints)*localScale.x,localScale.y,localScale.z);
+    }
+
+}
---
+++ animperium/Assets/UI/UI-Scripts/LookAtCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour {
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Camera.main.transform.rotation;
+
+    }
+}
