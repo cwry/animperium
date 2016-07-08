5d8a3be0c50bbcb27cc4c9bc770c27d689bcd823
cwry <daniel.pochert@gmail.com>
2016-07-08 06:59:00 +0200

    hole model / animation thingy

---
+++ animperium/Assets/Scripts/AutoDestroyParticle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroyParticle : MonoBehaviour {
+
+    public ParticleSystem ps;
+
+	void Update () {
+        if (ps) {
+            if (!ps.IsAlive()) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
