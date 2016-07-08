d1d4fba00840a1c64d36f226cdea6d6f1767bdb2
Fritz Lallinger <flallinger@googlemail.com>
2016-07-08 07:31:17 +0200

    +++

---
+++ animperium/Assets/UI/UI-Scripts/ResourcesText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResourcesText : MonoBehaviour {
+
+    public Text wood;
+    public Text iron;
+    public Text stone;
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        wood.text = Data.wood.ToString();
+        iron.text = Data.iron.ToString();
+        stone.text = Data.stone.ToString();
+    }
+    
+}
