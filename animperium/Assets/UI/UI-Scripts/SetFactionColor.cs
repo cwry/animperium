5d7bf57142d06e4d4515aeac21dae359176842b3
Fritz Lallinger <flallinger@googlemail.com>
2016-08-11 12:37:26 +0200

    letzter commit von meiner seite, danke für die schöne zeit :dancers:

---
+++ animperium/Assets/UI/UI-Scripts/SetFactionColor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SetFactionColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SetColor();
+	}
+
+    void SetColor() {
+        
+        Renderer[] renderer = GetComponentsInChildren<Renderer>();
+        List<Material> materials = new List<Material>();
+        foreach(Renderer r in renderer) {
+            if(r.sharedMaterial != null) {
+                materials.Add(r.sharedMaterial);
+            }
+        } 
+        foreach(Material m in materials) {
+            if(m.name == "fraktionsfarbe") {
+                if (Data.playerID == 1) m.SetColor("p1", Color.blue);
+                else m.SetColor("_DETAIL_MULX2", Color.red);
+            }
+        }
+    }
+}
