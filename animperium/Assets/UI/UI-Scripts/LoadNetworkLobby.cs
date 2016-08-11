25693172296daea3a423e20f6de8419d632ac7c4
Fritz Lallinger <flallinger@googlemail.com>
2016-08-11 09:27:23 +0200

    startlogo + diverse änderungen

---
+++ animperium/Assets/UI/UI-Scripts/LoadNetworkLobby.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadNetworkLobby : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(StartLobby());
+	}
+	
+	IEnumerator StartLobby() {
+        yield return new WaitForSeconds(12f);
+        SceneManager.LoadScene(2);
+    }
+}
