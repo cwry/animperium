a7b9081ac41503c1d2ccbed8d3b2f24380b6b7ec
Fritz Lallinger <flallinger@googlemail.com>
2016-08-11 08:01:48 +0200

    mir fallen keine commits mehr ein .... :'(

---
+++ animperium/Assets/UI/UI-Scripts/FadeIn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour {
+
+    private CanvasRenderer[] renders;
+    public float fadeSpeed = 3.91f;
+    // Use this for initialization
+    void Start () {
+        renders = gameObject.GetComponentsInChildren<CanvasRenderer>();
+        StartCoroutine(Fade());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator Fade() {
+        foreach (CanvasRenderer c in renders) {
+            c.SetAlpha(0f);
+        }
+        yield return 0;
+
+        while (renders[0].GetAlpha() < 1f) {
+            foreach (CanvasRenderer c in renders) {
+                c.SetAlpha(c.GetAlpha() + Time.deltaTime * fadeSpeed);
+            }
+            yield return 0;
+        }
+
+        yield break;
+    }
+}
